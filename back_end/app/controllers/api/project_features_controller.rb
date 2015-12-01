9d4062f094be96172d5a07f52919379d79a1004d
Yahor Zhylinski <yahor.zhylinski@gmail.com>
2015-12-01 22:29:57 +0300

    add rails-api

---
+++ back_end/app/controllers/api/project_features_controller.rb
@@ -0,0 +1,15 @@
+class Api::ProjectFeaturesController < Api::BaseController
+
+  PATH_TO_FEATURES = "cache_features"
+  TIME_TO_EXPIRE = 12.hours
+
+  def index 
+    project_features = nil
+    if !(project_features = Rails.cache.read(PATH_TO_FEATURES))
+      project_features =  ProjectFeature.includes(feature_prices: :project_types).all
+      Rails.cache.write(project_features, PATH_TO_FEATURES, expires_in: TIME_TO_EXPIRE)
+    end
+
+    render json: project_features
+  end
+end
